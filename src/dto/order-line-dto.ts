9f4c353f02b50b3e9246958a209006fe4852c708
Nilesh <nilesh.chauhan@pmcretail.com>
2019-09-13 12:42:33 +0530

    - refactor code to fetch customer order

--- src/dto/order-line-dto.ts
+++ src/dto/order-line-dto.ts
@@ -1,6 +1,8 @@
 export class OrderLineDto {
     id: number;
     productId: number;
+    productName: string;
+    price: number;
     quantity: number;
     customerId: number;
     orderId: number;
